4ae04a856c15d7e39ce75cb9dab655f6911cfd86
thiagocmachado@yahoo.com.br <thiagocmachado@yahoo.com.br>
2023-12-11 09:34:54 +0100

    simple jwt

--- Backend/users/api/router.py
+++ Backend/users/api/router.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from users.api.views import UserApiViewSet, UserView
 
 router_user = DefaultRouter()
@@ -11,5 +13,6 @@ router_user.register(
     )
 
 urlpatterns = [
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/me/', UserView.as_view()),
 ]
