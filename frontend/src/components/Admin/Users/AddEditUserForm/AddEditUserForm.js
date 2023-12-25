d6a6ea162a38fca6a8d5bba5426b46ae4f049840
thiagocmachado@yahoo.com.br <thiagocmachado@yahoo.com.br>
2023-12-25 12:59:08 +0100

    form validations

--- frontend/src/components/Admin/Users/AddEditUserForm/AddEditUserForm.js
+++ frontend/src/components/Admin/Users/AddEditUserForm/AddEditUserForm.js
@@ -1,49 +1,72 @@
 import React from "react";
 import { Form, Button, Checkbox } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import "./AddEditUserForm.scss";
 
 export function AddEditUserForm() {
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: Yup.object(newSchema()),
+        validateOnChange: false,
+        onSubmit: (formValue) => {
+            console.log("Formulario enviado");
+            console.log(formValue);
+        },
+    });
     return (
         <div>
-            <Form className="add-edit-user-form">
+            <Form className="add-edit-user-form" onSubmit={formik.handleSubmit}>
                 <Form.Input
                     name="username"
                     placeholder="Nombre de usuario"
-                    // onChange={}
+                    value={formik.values.username}
+                    onChange={formik.handleChange}
+                    error={formik.errors.username}
                 />
                 <Form.Input
                     name="email"
                     placeholder="Correo electrónico"
-                    // onChange={}
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.errors.email}
                 />
                 <Form.Input
-                    name="First_name"
+                    name="first_name"
                     placeholder="Nombre"
-                    // onChange={}
+                    value={formik.values.first_name}
+                    onChange={formik.handleChange}
+                    error={formik.errors.first_name}
                 />
                 <Form.Input
-                    name="Last_name"
+                    name="last_name"
                     placeholder="Apellido"
-                    // onChange={}
+                    value={formik.values.last_name}
+                    onChange={formik.handleChange}
+                    error={formik.errors.last_name}
                 />
                 <Form.Input
                     name="password"
                     placeholder="Contraseña"
                     type="password"
-                    // onChange={}
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    error={formik.errors.password}
                 />
 
                 <div className="Add-edit-user-form__active">
                     <Checkbox
                         toggle
-                        // onChange={}
+                        checked={formik.values.is_active}
+                        onChange={(e, data) => formik.setFieldValue("is_active", data.checked)}
                     />{" "}
                     Usuario activo
                 </div>
                 <div className="Add-edit-user-form__staff">
                     <Checkbox
                         toggle
-                        // onChange={}
+                        checked={formik.values.is_staff}
+                        onChange={(e, data) => formik.setFieldValue("is_staff", data.checked)}
                     />{" "}
                     Es miembro del staff
                 </div>
@@ -54,4 +77,28 @@ export function AddEditUserForm() {
             </Form>
         </div>
     );
+}
+
+function initialValues() {
+    return {
+        username: "",
+        email: "",
+        first_name: "",
+        last_name: "",
+        password: "",
+        is_active: true,
+        is_staff: false,
+    };
+}
+
+function newSchema() {
+    return {
+        username: Yup.string().required(true),
+        email: Yup.string().email(true).required(true),
+        first_name: Yup.string(),
+        last_name: Yup.string(),
+        password: Yup.string().required(true),
+        is_active: Yup.bool().required(true),
+        is_staff: Yup.bool().required(true),
+    };
 }
