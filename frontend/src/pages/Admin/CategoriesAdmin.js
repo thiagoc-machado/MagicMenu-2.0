a6b608cfea84bf9bf808c2ff20e19ae1dc040bbc
thiagocmachado@yahoo.com.br <thiagocmachado@yahoo.com.br>
2023-12-31 09:37:31 +0100

    categories

--- frontend/src/pages/Admin/CategoriesAdmin.js
+++ frontend/src/pages/Admin/CategoriesAdmin.js
@@ -4,7 +4,7 @@ import { HeaderPage } from "../../components/Admin";
 export function CategoriesAdmin() {
     return (
         <div>
-            <HeaderPage title="Categories" btnTitle="Nueva categoria"/>
+            <HeaderPage title="Categories" btnTitle="Crear una Nueva categoria"/>
         </div>
     );
 }
