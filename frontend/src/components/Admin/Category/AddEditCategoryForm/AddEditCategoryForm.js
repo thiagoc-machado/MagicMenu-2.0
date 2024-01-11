a9a66509c58d414ef0a1029ff8cec5d148b808c5
thiagocmachado@yahoo.com.br <thiagocmachado@yahoo.com.br>
2024-01-11 09:23:46 +0100

    categories form model

--- frontend/src/components/Admin/Category/AddEditCategoryForm/AddEditCategoryForm.js
+++ frontend/src/components/Admin/Category/AddEditCategoryForm/AddEditCategoryForm.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import {Form, Image, Button} from "semantic-ui-react"
-import "./AddEditCategoryForm.scss"
+import React from "react";
+import { Form, Image, Button } from "semantic-ui-react";
+import "./AddEditCategoryForm.scss";
 
 export function AddEditCategoryForm() {
-  return (
-    <Form className="add-edit-category-form">
-        <Form.Input name="titke" placeholder="Nombre de la categoria" />
-      <Button type="button" fluid>Subir image</Button>
-    </Form>
-  )
+    return (
+        <Form className="add-edit-category-form">
+            <Form.Input name="titke" placeholder="Nombre de la categoria" />
+            <Button type="button" fluid>
+                Subir image
+            </Button>
+            <Button type="submit" primary fluid content="Crear"></Button>
+        </Form>
+    );
 }
