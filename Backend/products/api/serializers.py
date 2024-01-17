b53589a0cf1e39f43c930c664cdd4e418f2f0c37
thiagocmachado@yahoo.com.br <thiagocmachado@yahoo.com.br>
2024-01-17 14:03:46 +0100

    backend products

--- Backend/products/api/serializers.py
+++ Backend/products/api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework.serializers import ModelSerializer
 from products.models import Product
+from categories.api.serializers import CategorySerializer
 
 class ProductSerializer(ModelSerializer):
+
+    category_data = CategorySerializer(source='category', read_only=True)
     class Meta:
         model = Product
-        fields = ['id', 'title', 'price', 'active', 'category']
+        fields = ['id', 'title', 'price', 'active', 'category', 'category_data']
         read_only_fields = ('id',) 
