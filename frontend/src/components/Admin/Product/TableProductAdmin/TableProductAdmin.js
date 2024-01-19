ddde2b74f49994d063d0d0920065d7cd7e819312
thiagocmachado@yahoo.com.br <thiagocmachado@yahoo.com.br>
2024-01-19 10:15:44 +0100

    products

--- frontend/src/components/Admin/Product/TableProductAdmin/TableProductAdmin.js
+++ frontend/src/components/Admin/Product/TableProductAdmin/TableProductAdmin.js
@@ -4,56 +4,60 @@ import { map } from "lodash";
 import "./TableProductAdmin.scss";
 
 export function TableProductAdmin(props) {
-    const { products } = props;
-    console.log(products);
+    const { products, updateProduct, deleteProduct } = props;
+
     return (
-        <div>
-            <Table className="table-product-admin">
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>Imagen</Table.HeaderCell>
-                        <Table.HeaderCell>Producto</Table.HeaderCell>
-                        <Table.HeaderCell>Precios</Table.HeaderCell>
-                        <Table.HeaderCell>Categoria</Table.HeaderCell>
-                        <Table.HeaderCell>Activo</Table.HeaderCell>
-                        <Table.HeaderCell></Table.HeaderCell>
+        <Table className="table-product-admin">
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell>Imagen</Table.HeaderCell>
+                    <Table.HeaderCell>Producto</Table.HeaderCell>
+                    <Table.HeaderCell>Precio</Table.HeaderCell>
+                    <Table.HeaderCell>Categoria</Table.HeaderCell>
+                    <Table.HeaderCell>Activo</Table.HeaderCell>
+                    <Table.HeaderCell></Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+                {map(products, (product, index) => (
+                    <Table.Row key={index}>
+                        <Table.Cell width={2}>
+                            <Image src={product.image} />
+                        </Table.Cell>
+                        <Table.Cell>{product.title}</Table.Cell>
+                        <Table.Cell>{product.price} €</Table.Cell>
+                        <Table.Cell>{product.category_data.title}</Table.Cell>
+                        <Table.Cell className="status">
+                            {product.active ? (
+                                <Icon name="check" />
+                            ) : (
+                                <Icon name="close" />
+                            )}
+                        </Table.Cell>
+
+                        <Actions
+                            product={product}
+                            updateProduct={updateProduct}
+                            deleteProduct={deleteProduct}
+                        />
                     </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {map(products, (product, index) => (
-                        <Table.Row key={index}>
-                            <Table.Cell width={2}>
-                                <Image src={product.image} />
-                            </Table.Cell>
-                            <Table.Cell>{product.title}</Table.Cell>
-                            <Table.Cell>{product.price} €</Table.Cell>
-                            <Table.Cell>
-                                {product.category_data.title}
-                            </Table.Cell>
-                            <Table.Cell className="status">
-                                {product.active ? (
-                                    <Icon name="check" />
-                                ) : (
-                                    <Icon name="close" />
-                                )}
-                            </Table.Cell>
-                            <Actions product={product}/>
-                        </Table.Row>
-                    ))}
-                </Table.Body>
-            </Table>
-        </div>
+                ))}
+            </Table.Body>
+        </Table>
     );
 }
 
 function Actions(props) {
+    const { product, updateProduct, deleteProduct } = props;
+
     return (
         <Table.Cell textAlign="right">
-            <Button icon onClick={() => console.log("Editar...")}>
+            <Button icon onClick={() => updateProduct(product)}>
                 <Icon name="pencil" />
             </Button>
-            <Button icon onClick={() => console.log("Eliminar...")}>
-                <Icon negative name="close" />
+            <Button icon negative onClick={() => deleteProduct(product)}>
+                <Icon name="close" />
             </Button>
         </Table.Cell>
     );
--- frontend/src/components/Admin/Product/index.js
+++ frontend/src/components/Admin/Product/index.js
@@ -1,1 +1,2 @@
 export { TableProductAdmin } from "./TableProductAdmin";
+export * from "./AddEditProductForm";
