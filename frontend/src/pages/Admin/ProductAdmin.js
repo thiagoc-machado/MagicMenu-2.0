1e67e7a0a564d4f53708cd4158ac39f13a4d3dc2
thiagocmachado@yahoo.com.br <thiagocmachado@yahoo.com.br>
2024-01-27 23:54:33 +0100

    products

--- frontend/src/pages/Admin/ProductAdmin.js
+++ frontend/src/pages/Admin/ProductAdmin.js
@@ -32,7 +32,7 @@ export function ProductAdmin() {
     const onRefetch = () => setRefetch((prev) => !prev);
 
     const addProduct = () => {
-        setTitleModal("crear nuevo producto");
+        setTitleModal("crear producto");
         setContentModal(
             <AddEditProductForm
                 onClose={openCloseModal}
